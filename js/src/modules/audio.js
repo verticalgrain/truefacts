bbb93b53415368c05b6ac61a24573e0002e5c359
Jamie Graham <jamie@domain7.com>
2015-06-19 09:45:49 -0700

    Audio list now in JSON. Added ding sound.

--- js/src/modules/audio.js
+++ js/src/modules/audio.js
@@ -6,20 +6,22 @@ app.audio = (function($){
 	playFacts;
 
 	function init(){
-		playFacts = setInterval( function() {getAudioFiles(audioFiles)},10000);
+		playAudio('ding.mp3');
+		getAudioFilesJson(audioFiles);
+		playFacts = setInterval( function() {getAudioFilesJson(audioFiles)},20000);
 		pauseFacts();
 	}
 
 	var audioFiles = [];
 
 	function playAudio(audioFile) {
-		var audio = new Audio('../audio/'+audioFile);
+		var audio = new Audio('../../audio/'+audioFile);
 		audio.play();
 	}
 
 	function getAudioFiles(audioSources) {
 		$.ajax({
-		    url: '../../audio/',
+		    url: 'audio/',
 		    success: function (data) {
 		       	$(data).find('a:contains("m4a")').each(function () {
 		       		var filename = $(this).attr('href');
@@ -29,8 +31,23 @@ app.audio = (function($){
 			    chooseRandomAudio(audioSources);
 		    }
 		
-		});		
+		});
 	}
+
+
+	function getAudioFilesJson(audioSources) {
+		$.getJSON( 'json/audiofiles.json', function(audioList) {
+			$.each( audioList.audiofiles, function(i, audiofile){
+				audioSources.push(audiofile.filename);
+			});
+
+		    chooseRandomAudio(audioSources);
+
+		});
+	}
+
+
+
 
 	function chooseRandomAudio(audioSources) {
 		rand = audioSources[Math.floor(Math.random() * audioSources.length)];
@@ -40,7 +57,6 @@ app.audio = (function($){
 	function pauseFacts() {
 		$('.pausefacts').on('click',function() {
 			window.clearTimeout(playFacts);
-			console.log('facts have been paused');
 		});
 	}
 
