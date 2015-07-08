dd17aac6557b67f14676735415d646ed037b9e30
Jamie Graham <jamie@domain7.com>
2015-07-08 09:25:25 -0700

    Added time indicator

--- js/src/modules/audio.js
+++ js/src/modules/audio.js
@@ -5,26 +5,68 @@ app.audio = (function($){
 	audio,
 	ding,
 	rand,
-	playFacts;
+	playFacts,
+	playTime;
+
+	playTime = 40000;
+
 
 	function init(){
 		var ding = new Audio('audio/ding.mp3');
 		ding.volume = 0.2;
+		// Play ding sound
 		ding.play();
+		// Play first fact
+		// setTimeout( function() { getAudioFilesJson(audioFiles)},2000);
+		// Start playing facts
 		getAudioFilesJson(audioFiles);
-		playFacts = setInterval( function() {getAudioFilesJson(audioFiles)},30000);
-		pauseFacts();
+		playFacts = setInterval( function() {getAudioFilesJson(audioFiles)},playTime);
 	}
 
 	var audioFiles = [];
 
-	function playAudio(audioFile) {
-		var audio = new Audio('audio/'+audioFile);
-		// audio.volume = 0.8;
-		audio.play();
+	// Get the json source of audio files
+	function getAudioFilesJson(audioSources) {
+		$.getJSON( 'json/audiofiles.json', function(audioList) {
+			$.each( audioList.audiofiles, function(i, audiofile){
+				audioSources.push(audiofile.filename);
+			});
+
+		    chooseRandomAudio(audioSources);
+
+		});
 	}
 
 
+	// Play a random audio file
+	function chooseRandomAudio(audioSources) {
+		rand = audioSources[Math.floor(Math.random() * audioSources.length)];
+		playAudio(rand);
+	}
+
+
+	//Play the audio files
+	function playAudio(audioFile) {
+		$('body').removeClass('is-progressing');
+
+		$('body').addClass('is-progressing');
+
+		var audio = new Audio('audio/'+audioFile);
+		// audio.volume = 0.8;
+		audio.play();
+
+		$('.pausefacts').on('click',function() {
+			
+			$('body').toggleClass('is-paused');
+
+			// window.clearTimeout(playFacts);
+
+			audio.pause();
+		});
+	}
+
+
+	// Scan directory to get all audio files (deprecated)
 	function getAudioFiles(audioSources) {
 		$.ajax({
 		    url: 'audio/',
@@ -41,28 +83,6 @@ app.audio = (function($){
 	}
 
 
-	function getAudioFilesJson(audioSources) {
-		$.getJSON( 'json/audiofiles.json', function(audioList) {
-			$.each( audioList.audiofiles, function(i, audiofile){
-				audioSources.push(audiofile.filename);
-			});
-
-		    chooseRandomAudio(audioSources);
-
-		});
-	}
-
-
-	function chooseRandomAudio(audioSources) {
-		rand = audioSources[Math.floor(Math.random() * audioSources.length)];
-		playAudio(rand);
-	}
-
-	function pauseFacts() {
-		$('.pausefacts').on('click',function() {
-			window.clearTimeout(playFacts);
-		});
-	}
 
 	/* Document ready
 	/* + + + + + + + + + + + + + + + + + + + + + + + + + + + */
